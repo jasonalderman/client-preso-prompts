aeaea4b0c958b6b7e0ed5b7089059afe83ed8df6
Jason Alderman <jasonald@gmail.com>
2017-04-03 22:14:41 -0700

    Update madlibs.js.

--- madlibs.js
+++ madlibs.js
@@ -1,13 +1,16 @@
 var madlibs =
 {
-	"what": [
+  "what": [
     "website",
     "mobile website",
     "iPhone app",
     "Android app",
-    "watch app"
+    "watch app",
+    "internal tools",
+    "taxonomy",
+    "brand-wide design system"
   ],
-	"who": [
+  "who": [
     "McDonald's",
     "Taco Bell",
     "the New York Times",
@@ -15,15 +18,20 @@ var madlibs =
     "Netflix",
     "Kleenex",
     "Pampers",
+    "Charmin",
+    "Chobani Yogurt",
     "Walgreens",
     "Amazon",
     "eBay",
     "Chevrolet",
     "Target"
   ],
-	"why": [
+  "why": [
     "improve accessibility for blind customers",
-    "make it easier to use with voice commands",
+    "make it easier to use hands-free",
+    "reduce the amount of email sent",
+    "encourage mindfulness and contemplation",
+    "incentivize brand loyalty",
     "reduce the steps it takes to talk to customer service on the phone",
     "reduce the amount of ads delivered to the user",
     "make it easier to find relevant reviews and comments",
@@ -34,10 +42,11 @@ var madlibs =
     "the people who designed their current #product#",
     "the CEO of #client#",
     "the CFO of #client#",
-    "the stakeholders at #client# who will lose their job if the new #product# fails",
-    "a skype call full of investors",
+    "the regional manager of #client#, for Southern California",
+    "the stakeholders at #client# who will lose their jobs if the new #product# fails",
+    "a skype-call full of investors",
     "your grandmother, who is VERY SERIOUS about #client#"
   ],
-	"story": ['You’ve redesigned the <span id="product">#product#</span> of <span id="client">#client#</span> in order to <span id="why">#why#</span>, and you need to justify your design decisions to <span id="whom">#whom#</span>.'],
-	"origin": ["#[product:#what#][client:#who#]story#"]
+  "story": ['You’ve redesigned the <span id="product">#product#</span> of <span id="client">#client#</span> in order to <span id="why">#why#</span>, and you need to justify your design decisions to <span id="whom">#whom#</span>.'],
+  "origin": ["#[product:#what#][client:#who#]story#"]
 };
