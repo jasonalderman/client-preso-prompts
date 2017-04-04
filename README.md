f3236d3eb14fd28779537122993a4a14f758644a
Jason Alderman <jasonald@gmail.com>
2017-04-03 21:56:14 -0700

    First commit.

---
+++ README.md
@@ -0,0 +1,3 @@
+# Client Presentation Mad Libs
+
+Prompts for the 5 April 2017 UX Speakeasy meetup on _Presenting to Clients._
---
+++ madlibs.js
@@ -0,0 +1,43 @@
+var madlibs =
+{
+	"what": [
+    "website",
+    "mobile website",
+    "iPhone app",
+    "Android app",
+    "watch app"
+  ],
+	"who": [
+    "McDonald's",
+    "Taco Bell",
+    "the New York Times",
+    "Fox News",
+    "Netflix",
+    "Kleenex",
+    "Pampers",
+    "Walgreens",
+    "Amazon",
+    "eBay",
+    "Chevrolet",
+    "Target"
+  ],
+	"why": [
+    "improve accessibility for blind customers",
+    "make it easier to use with voice commands",
+    "reduce the steps it takes to talk to customer service on the phone",
+    "reduce the amount of ads delivered to the user",
+    "make it easier to find relevant reviews and comments",
+    "make it easier to purchase",
+    "make it more delightful"
+  ],
+  "whom": [
+    "the people who designed their current #product#",
+    "the CEO of #client#",
+    "the CFO of #client#",
+    "the stakeholders at #client# who will lose their job if the new #product# fails",
+    "a skype call full of investors",
+    "your grandmother, who is VERY SERIOUS about #client#"
+  ],
+	"story": ['You’ve redesigned the <span id="product">#product#</span> of <span id="client">#client#</span> in order to <span id="why">#why#</span>, and you need to justify your design decisions to <span id="whom">#whom#</span>.'],
+	"origin": ["#[product:#what#][client:#who#]story#"]
+};
